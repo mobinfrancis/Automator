702706853c74add38c2b9d82fbf0950c95b1afbb
Sumon <sdtechie22@gmail.com>
2021-08-11 08:36:31 +0530

    Optimized the browser configurations

--- src/main/java/com/automator/controllers/BrowserController.java
+++ src/main/java/com/automator/controllers/BrowserController.java
@@ -8,8 +8,8 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BrowserController {
@@ -31,33 +31,56 @@ public class BrowserController {
 		}
 		log.info("Chromedriver path: " + driverPath + chromeDriverName);
 		System.setProperty("webdriver.chrome.driver", driverPath + chromeDriverName);
-		DisplayMode displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
-				.getDisplayMode();
-		int screen_width = (int) displayMode.getWidth();
-		int screen_height = (int) displayMode.getHeight();
-		log.info("Desktop Screen Resolution: " + screen_width + "/" + screen_height);
+		displayDesktopScreenResolution();
 		driver = new ChromeDriver(getChromeOptions());
-		log.info("Running Chrome Browser Dimension: " + driver.manage().window().getSize());
+		displayBrowserDimension(driver, "Chrome");
 		return driver;
 	}
 
 	public WebDriver firefoxSetUp(WebDriver driver) {
-		System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
+		String firefoxDriverName = "geckodriver.exe";
+		configController = new ConfigController();
+		if (configController.doesSystemPropertyConfigExistFor("DRIVER_LOCATION")) {
+			driverPath = System.getProperty("DRIVER_LOCATION");
+		}
+		if (configController.doesSystemPropertyConfigExistFor("macOS")) {
+			if (System.getProperty("macOS").equals("true")) {
+				firefoxDriverName = "geckodriver";
+			}
+		}
+		log.info("Geckodriver path: " + driverPath + firefoxDriverName);
+		System.setProperty("webdriver.gecko.driver", driverPath + firefoxDriverName);
+		displayDesktopScreenResolution();
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		displayBrowserDimension(driver, "Firefox");
 		return driver;
 	}
 
 	public WebDriver edgeSetUp(WebDriver driver) {
-		System.setProperty("webdriver.edge.driver", driverPath + "MicrosoftWebDriver.exe");
-		driver = new InternetExplorerDriver();
+		String edgeDriverName = "msedgedriver.exe";
+		configController = new ConfigController();
+		if (configController.doesSystemPropertyConfigExistFor("DRIVER_LOCATION")) {
+			driverPath = System.getProperty("DRIVER_LOCATION");
+		}
+		if (configController.doesSystemPropertyConfigExistFor("macOS")) {
+			if (System.getProperty("macOS").equals("true")) {
+				edgeDriverName = "msedgedriver";
+			}
+		}
+		System.setProperty("webdriver.edge.driver", driverPath + edgeDriverName);
+		displayDesktopScreenResolution();
+		driver = new EdgeDriver();
 		driver.manage().window().maximize();
+		displayBrowserDimension(driver, "Edge");
 		return driver;
 	}
 
 	public WebDriver safariSetUp(WebDriver driver) {
+		displayDesktopScreenResolution();
 		driver = new SafariDriver();
 		driver.manage().window().maximize();
+		displayBrowserDimension(driver, "Safari");
 		return driver;
 	}
 
@@ -76,6 +99,18 @@ public class BrowserController {
 			}
 		}
 		return chromeOptions;
+	}
+
+	public void displayDesktopScreenResolution() {
+		DisplayMode displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+				.getDisplayMode();
+		int screen_width = (int) displayMode.getWidth();
+		int screen_height = (int) displayMode.getHeight();
+		log.info("Desktop Screen Resolution: " + screen_width + "/" + screen_height);
+	}
+
+	public void displayBrowserDimension(WebDriver driver, String browser) {
+		log.info("Running " + browser +" Browser Dimension: " + driver.manage().window().getSize());
 	}
 
 }
